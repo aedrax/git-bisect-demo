52f7e63ace7c6fc79b8158c660ad58b28c54045c
Paul Sorensen <paulsorensen5@gmail.com>
2024-12-18 14:08:53 -0500

    Add the modulus function

--- calculator.py
+++ calculator.py
@@ -18,10 +18,15 @@ def divide(a, b):
 def pow(a, b):
     return a ** b
 
+def modulus(a, b): 
+    return a % b
+
 if __name__ == "__main__":
     print("Add: ", add(2,3))
     print("Sub: ", subtract(2,3))
     print("Mult: ", mult(2,3))
     print("Divide: ", divide(10, 2))
     print("Power: ", pow(5, 4))
+    print("Modulus: ", modulus(12, 5))
+
 
