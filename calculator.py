0b4dbdf3ffc1f3fcdaeb527af72b41630b811a58
Paul Sorensen <paulsorensen5@gmail.com>
2024-12-18 14:04:29 -0500

    Add the multiply function

--- calculator.py
+++ calculator.py
@@ -6,6 +6,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def mult(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Add: ", add(2,3))
     print("Sub: ", subtract(2,3))
+    print("Mult: ", mult(2,3))
