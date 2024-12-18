08d1ff627b5db147844d4f90cce7be6163b81864
Paul Sorensen <paulsorensen5@gmail.com>
2024-12-18 14:05:51 -0500

    Add the division function

--- calculator.py
+++ calculator.py
@@ -9,7 +9,14 @@ def subtract(a, b):
 def mult(a, b):
     return a * b
 
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    
+    return a / b
+
 if __name__ == "__main__":
     print("Add: ", add(2,3))
     print("Sub: ", subtract(2,3))
     print("Mult: ", mult(2,3))
+    print("Divide: ", divide(10, 2))
